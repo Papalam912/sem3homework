2a0d2e451a0277a0734217c7e76ffb68185f8acd
e.polovinkin <polkill@list.ru>
2022-11-10 17:35:38 +0300

    com2

--- Program.cs
+++ Program.cs
@@ -4,25 +4,25 @@ Console.Clear();
 
 // Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
 
-// Console.Write("введите пятизначное число: ");
-// int num = Convert.ToInt32(Console.ReadLine());
-// while (num < 10000 || num > 99999)
-// {
-//     Console.Write("вы ввели не пятизначное число, повторите ввод: ");
-//     int num2 = Convert.ToInt32(Console.ReadLine());
-//     if (num2 > 9999 || num2 < 100000)
-//     {
-//         num = num2;
-//     }
-// }
-// if (num / 10000 == num % 10 || num / 10000 % 10 == num / 10 % 10)
-// {
-//     Console.WriteLine($"Число {num} являеться палиндромом");
-// }
-// else
-// {
-//     Console.WriteLine($"число {num} не являеться палиндромом");
-// }
+Console.Write("введите пятизначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+while (num < 10000 || num > 99999)
+{
+    Console.Write("вы ввели не пятизначное число, повторите ввод: ");
+    int num2 = Convert.ToInt32(Console.ReadLine());
+    if (num2 > 9999 || num2 < 100000)
+    {
+        num = num2;
+    }
+}
+if (num / 10000 == num % 10 || num / 10000 % 10 == num / 10 % 10)
+{
+    Console.WriteLine($"Число {num} является палиндромом");
+}
+else
+{
+    Console.WriteLine($"число {num} не является палиндромом");
+}
 
 
 // Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
